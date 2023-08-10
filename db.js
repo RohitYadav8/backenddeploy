dc03acfcfd74b23ebde8c8077feaa2f107521fc6
RohitYadav8 <rohityadav40823@gmail.com>
2023-08-10 17:06:12 +0700

    some changes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 require("dotenv").config()
-const connection=mongoose.connect(process.env.mongoUrl)
+const connection=mongoose.connect(process.env.mongourl)
 
 module.exports={
     connection,
